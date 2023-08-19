1518123dedd361ec692f83e700c6060733e11639
kay-mastermind <hermandre32@gmail.com>
2023-08-19 17:16:28 +0000

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * get_flags - Calculates all active flags
+ * @format: A formatted string that prints the arguments
+ * @i: take a parameter
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
